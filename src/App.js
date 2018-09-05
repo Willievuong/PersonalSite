2597d0603b4a59b75feadb312359c2b9215df4c9
William Vuong <wpvuong@ucsd.edu>
2018-09-05 12:11:21 -0700

    Adding CSS and testing html from former site

--- src/App.js
+++ src/App.js
@@ -4,17 +4,36 @@ import './App.css';
 
 class App extends Component {
   render() {
-    return (
+    /* return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          
         </p>
       </div>
     );
+    */
+   /* Old CSS From Former Site */ 
+   return(
+    <div class="site-wrapper">
+    <div class="site-wrapper-inner">
+      <div class="cover-container">
+        <div class="masthead clearfix">
+          <div class="inner">
+              <h4 class="masthead-brand">Willievuong</h4>
+          </div>
+        </div>
+          <div class="inner cover">
+            <h1 class="cover-heading">Hey there!</h1>
+              <p class="lead">The site is under construction, check back in a few days for updates, meanwhile, here's my resume!</p>
+              <button href="https://1drv.ms/f/s!AhgK9ZtBhYlypqJaL95HYc1zRQcBOQ">Resume</button>
+          </div>
+          </div>
+      </div>
+    </div>
+   ); 
   }
 }
 
