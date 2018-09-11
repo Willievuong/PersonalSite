f45cfa47816246505d05c7dc84c2c6acb68115c8
William Vuong <wpvuong@ucsd.edu>
2018-09-11 15:41:40 -0700

    Cleaned Up CSS Files & Configuring Headers

--- src/App.js
+++ src/App.js
@@ -4,36 +4,43 @@ import './App.css';
 
 class App extends Component {
   render() {
-    /* return (
+    return (
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Hi, Hello, Welcome!</h1>
         </header>
         <p className="App-intro">
-          
+          The site is under construction, check back in a few days for updates, meanwhile, here's my resume!
         </p>
+        {/* TODO: Handle Button OnClick */}
+        <button className="Resume-Button" href="https://1drv.ms/f/s!AhgK9ZtBhYlypqJaL95HYc1zRQcBOQ"> 
+          Resume
+        </button>
       </div>
     );
-    */
+  
+   /*------------------------- */
    /* Old CSS From Former Site */ 
-   return(
-    <div class="site-wrapper">
-    <div class="site-wrapper-inner">
-      <div class="cover-container">
-        <div class="masthead clearfix">
-          <div class="inner">
-              <h4 class="masthead-brand">Willievuong</h4>
-          </div>
-        </div>
-          <div class="inner cover">
-            <h1 class="cover-heading">Hey there!</h1>
-              <p class="lead">The site is under construction, check back in a few days for updates, meanwhile, here's my resume!</p>
-              <button href="https://1drv.ms/f/s!AhgK9ZtBhYlypqJaL95HYc1zRQcBOQ">Resume</button>
-          </div>
-          </div>
-      </div>
-    </div>
-   ); 
+   /* TODO: Figure out which are from Bootstrap component */
+   /*--------------------------*/
+  //  return(
+  //   <div className="site-wrapper">
+  //   <div className="site-wrapper-inner">
+  //     <div className="cover-container">
+  //       <div className="masthead clearfix">
+  //         <div className="inner">
+  //             <h4 className="masthead-brand">Willievuong</h4>
+  //         </div>
+  //       </div>
+  //         <div className="inner cover">
+  //           <h1 className="cover-heading">Hey there!</h1>
+  //             <p className="lead">The site is under construction, check back in a few days for updates, meanwhile, here's my resume!</p>
+  //             <button href="https://1drv.ms/f/s!AhgK9ZtBhYlypqJaL95HYc1zRQcBOQ">Resume</button>
+  //         </div>
+  //         </div>
+  //     </div>
+  //   </div>
+  //  );  
   }
 }
 
