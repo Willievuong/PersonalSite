8bd9ef38023d9047e3a9bc0aeb090c2105c3fd57
William Vuong <wpvuong@ucsd.edu>
2018-09-20 17:28:52 -0700

    Adding hamburger package and redoing header file

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from "react-router-dom";
+import { slide as Menu } from 'react-burger-menu'
 import '../App.css';
 
 /* Header and Navigation Bar */
@@ -9,27 +10,31 @@ class HeaderPage extends Component{
     super(); 
   }
 
+  // showSettings(events){
+  //   event.preventDefault();
+  // }
+
+  getNavBar(){
+    let items; 
+
+    items = [
+      <li className="navButton"><Link to="/project">Project</Link></li>,
+      <li className="navButton"><Link to="/about">&nbsp;About&nbsp;</Link></li>,
+      <li className="navButton"><Link to="/">&nbsp;&nbsp;Home&nbsp;&nbsp;</Link></li>
+    ];
+  }
+
   render(){
-    const hamburger = () => {
-      // var x = document.getElementById("myTopnav");
-      // if (x.className === "topnav") {
-      //     x.className += " responsive";
-      // } else {
-      //     x.className = "topnav";
-      // }
-    }
-    
     return(
-    <header className="App-header">
-      <ul className="navbar" id="idNavBar">
-        <li><Link to="/project" className="navButton">Project</Link></li>
-        <li><Link to="/about" className="navButton">&nbsp;About&nbsp;</Link></li>
-        <li><Link to="/" className="navButton">&nbsp;&nbsp;Home&nbsp;&nbsp;</Link></li>
-        <li><a href="javascript:void(0);" className="icon" onClick={hamburger} >
-          <i className="fa fa-bars"></i>
-        </a></li>
-      </ul>
-    </header>
+    //<Menu> 
+      <header className="App-header">
+        <ul className="navbar">
+          <li><Link to="/project" className="navButton">Project</Link></li>
+          <li><Link to="/about" className="navButton">&nbsp;About&nbsp;</Link></li>
+          <li><Link to="/" className="navButton">&nbsp;&nbsp;Home&nbsp;&nbsp;</Link></li>
+        </ul>
+      </header>
+    //</Menu>
     );
   }
 //)
