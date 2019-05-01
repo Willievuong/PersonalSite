2751a153775948bc7aeaca515f821f9c4c46f079
William Vuong <wpvuong@ucsd.edu>
2019-04-30 23:57:12 -0700

    Model for blog

--- backend/blogs/models.py
+++ backend/blogs/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Difficulty(models.Model):
+    difficulty_id = models.CharField(max_length=10, primary_key=True)
+    name = models.CharField(max_length=200)
+
+class Blog(models.Model):
+
+    blog_id = models.CharField(max_length=25, unique=True)
+    title = models.TextField(verbose_name = 'Blog Title')
+    published = models.BooleanField(default=False)
+    publish_date = models.DateField(auto_now=True)
+    difficulty = models.ForeignKey(Difficulty, on_delete=models.CASCADE)
+    thumbnail = models.ImageField(upload_to='images/thumbnails')
+    medium_url = models.URLField()
+  
