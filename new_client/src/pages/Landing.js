1f625b081a0461fbbb06da927784e7b6958fb2c9
willievuong <willievuong@gmail.com>
2020-02-13 22:41:28 -0800

    completed introduction

--- new_client/src/pages/Landing.js
+++ new_client/src/pages/Landing.js
@@ -16,12 +16,36 @@ function Landing(){
     <div className="page">
         <div>
             <Avatar src={face} alt="face" size={128} />
-            <h1>William Vuong</h1>
         </div>
-        <div>
-            <p>A work in progress ;)</p>
-            <Typing strings={typeString}
-          />
+        <div className="biography">
+            <p>
+                I'm <b>William Vuong</b>, currently I am the lead app developer at 
+                <a href="https://theinfopost.com/"> Infopost </a> 
+                and co-Founder at 
+                <a href="https://wistly.io/"> Wistly. </a> 
+                Currently I reside in San Diego, but I consider home to be San Jose. 
+                Prior to working at Infopost and Wistly, I studied 
+                Mathematics Computer Science, a multi-disciplinary major at 
+                <a href="https://ucsd.edu/"> UC San Diego </a>. 
+            </p>
+            <p>
+                Generally speaking, I wear many hats, like <b>frontend</b> and <b>backend</b>. I find a lot of joy and passion
+                building beautiful and functional apps. Its really rewarding for me to create and bring life from design and idea
+                into a product that is alive and useful to others.  
+            </p>
+            <p>
+                When I'm not coding, I like to read and research random topics, and as a result I have accummulated a wealth
+                of trivia knowledge that I'm not sure what to do with. But hey, I really enjoy it! And throughout now and 
+                the future I'll be showcasing my library of knowledge here.  
+            </p>
+
+            <p>
+                I love meeting people and learning something new. So if you'd like to have a chat, I'd love to hear from you!
+                You can find out the various ways to reach me in the contact section. 
+            </p>
+            <p>
+                <Typing strings={typeString}/>
+            </p>
         </div>
         <div>
         <a href="https://www.linkedin.com/in/vuongwilliam/">
--- new_client/src/pages/WorkInProgress.js
+++ new_client/src/pages/WorkInProgress.js
@@ -2,39 +2,15 @@ import React from 'react';
 import { Icon, Button, Avatar } from "antd";
 
 import '../App.css';
-import face from "../assets/photo.jpg";
 
+import Typing from '../components/Typing'
 
 function WorkInProgressPage(){
-    const resume = "https://1drv.ms/f/s!AhgK9ZtBhYlypqJaL95HYc1zRQcBOQ";
-    return (
-    <div className="content">
-        <div className="face">
-            <Avatar src={face} alt="face" size={128} />
-            <h1>William Vuong</h1>
-        </div>
-        <div>
-            <p>A work in progress ;)</p>
-        </div>
-        <div>
-        <a href="https://www.linkedin.com/in/vuongwilliam/">
-            <Icon type="linkedin" />
-        </a>
-        <a href="https://github.com/willievuong">
-            <Icon type="github" />
-        </a>
-        <a href="mailto:willievuong@gmail.com">
-            <Icon type="mail" />
-        </a>
-        </div>
+    const typeString = ["Whoops!", "this section is under construction", "check back in a few weeks!"]
 
-        <Button
-            onClick={() => window.open(resume, "_blank")}
-            type="primary"
-            shape="round"
-            >
-            Download Resume
-        </Button>
+    return (
+    <div className="page">
+        <Typing strings={typeString}/>
     </div>
     );
 }
