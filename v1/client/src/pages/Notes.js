eb58dfed2f7d28e2365dc58c0c1e06d956943464
willievuong <willievuong@gmail.com>
2020-05-11 21:47:29 -0700

    Updating manifest and added card to notes

--- v1/client/src/pages/Notes.js
+++ v1/client/src/pages/Notes.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Button } from "antd";
+import { Card, Row, Col } from 'antd'
 
 import Typing from '../components/Typing'
 import '../App.css'
@@ -7,10 +8,50 @@ import '../App.css'
 
 function Notes(){
     const typeString = ["All my notes are written on medium!"]
+
+    const fillerCards = [
+        {   
+            "id": 1,
+            "title": "Sample 1",
+            "content": "Content of a Card"
+        },
+        {
+            "id": 2,
+            "title": "Sample 2",
+            "content": "Content of a Card"
+        },
+        {
+            "id": 3,
+            "title": "Sample 3",
+            "content": "Content of a Card"
+        },
+    ]
     
+    function cardBuilder(data){
+        let { title, content } = data
+        return(
+            <Col span={8}>
+                <Card className="projectCard" key={data.id} title={title}>
+                    <p>{content}</p>
+                </Card>
+            </Col>
+        )
+    }
+
+
     return(
         <div className="page">
-            <Typing strings={typeString}/>
+            <div>
+                Notes    
+            </div>
+
+            <div>
+                <Typing strings={typeString}/>
+            </div>
+
+            <Col>
+                {fillerCards.map((data) => cardBuilder( data ))}
+            </Col>
             
         </div>
     )
